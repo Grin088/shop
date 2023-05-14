df15e854443149b0aa96212ea7201c2342548aa3
Egor Tarasov <egorka.tarasov.20001@gmail.com>
2023-05-15 02:17:02 +0300

    connection admin

--- market/products/admin.py
+++ market/products/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin  # noqa F401
 
-# Register your models here.
+from .models import Product, ProductProperty, Property
+
+
+class ProductPropertyInline(admin.TabularInline):
+    model = Product.property.through
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductPropertyInline,
+    ]
+    list_display = 'name',
+
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = 'name',
+
+
+@admin.register(ProductProperty)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = 'product', 'property', 'value',
--- market/shops/admin.py
+++ market/shops/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin  # noqa F401
 
-# Register your models here.
+from .models import Shop, Offer
+
+
+class ShopProductInline(admin.TabularInline):
+    model = Shop.products.through
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    inlines = [
+        ShopProductInline,
+    ]
+    list_display = 'name',
+
+
+@admin.register(Offer)
+class OfferAdmin(admin.ModelAdmin):
+
+    list_display = 'shop', 'product', 'price',
