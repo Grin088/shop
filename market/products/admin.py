60b38f9ba1ddf8943dddc3ad21a8679b66aac067
??????? ??????? <Evgeniykurdeev@mail.ru>
2023-05-15 18:50:10 +0300

    Adding images to the product

--- market/products/admin.py
+++ market/products/admin.py
@@ -1,18 +1,21 @@
 from django.contrib import admin  # noqa F401
 
-from .models import Product, ProductProperty, Property
+from .models import Product, ProductProperty, Property, ProductImage
 
 
 class ProductPropertyInline(admin.TabularInline):
     model = Product.property.through
 
+class ProductProductImageInline(admin.TabularInline):
+    model = ProductImage
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     inlines = [
+        ProductProductImageInline,
         ProductPropertyInline,
     ]
-    list_display = 'name',
+    list_display = 'name', 'preview'
 
 
 @admin.register(Property)
