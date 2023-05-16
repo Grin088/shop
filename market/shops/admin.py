9d4a5a9cc73f747713c616f730cfec5fcd8c6618
Zhibina <taisj281936@gmail.com>
2023-05-16 20:54:42 +0300

    Creating an admin panel to manage banners

--- market/shops/admin.py
+++ market/shops/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin  # noqa F401
 
-from .models import Shop, Offer
+from .models import Shop, Offer, Banner
 
 
 class ShopProductInline(admin.TabularInline):
@@ -18,3 +18,12 @@ class ShopAdmin(admin.ModelAdmin):
 @admin.register(Offer)
 class OfferAdmin(admin.ModelAdmin):
     list_display = 'shop', 'product', 'price',
+
+
+@admin.register(Banner)
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'image', 'link', 'start_date', 'end_date', 'active')
+    list_filter = ('active',)
+    search_fields = ('title', 'link')
+    ordering = ('-end_date',)
+
