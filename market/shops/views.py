4873328cff588f6d1f429baf7498b4cabf16ca4f
Zhibina <taisj281936@gmail.com>
2023-05-16 21:32:04 +0300

    Added function to display three random banners

--- market/shops/views.py
+++ market/shops/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render  # noqa F401
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Banner
+from django.conf import settings
+from django.views.decorators.cache import cache_page
+
+
+@cache_page(600)
+def home(request):
+    """Функция для главной страницы для вывода трёх случайных активных баннеров. Баннеры закешированы на десять минут"""
+    random_banners = Banner.objects.filter(active=True).order_by('?')[:3]
+
+    context = {
+        'random_banners': random_banners,
+    }
+    return render(request, 'banner.html', context)
