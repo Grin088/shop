2d5289484e6b1c61c24f42177a0bf19287b5ae63
Zhibina <taisj281936@gmail.com>
2023-05-17 15:22:54 +0300

    imported constant from the settings module

--- market/shops/views.py
+++ market/shops/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render  # noqa F401
-
-from django.shortcuts import render
 from .models import Banner
 from django.conf import settings
 from django.views.decorators.cache import cache_page
 
 
-@cache_page(600)
+@cache_page(settings.CACHE_CONSTANT)
 def home(request):
-    """Функция для главной страницы для вывода трёх случайных активных баннеров. Баннеры закешированы на десять минут"""
+    """Функция для главной страницы для вывода трёх случайных активных баннеров.
+     Баннеры закешированы на десять минут"""
     random_banners = Banner.objects.filter(active=True).order_by('?')[:3]
 
     context = {
