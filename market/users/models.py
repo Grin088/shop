2d1c5c9f92b133afc2cdfdf7a04ae9e6aecaa755
Grin_9 <Grin_9@mail.ru>
2023-05-19 01:29:26 +1200

    add_user_profile_model_add_view_login_logout_registration

---
+++ market/users/models.py
@@ -0,0 +1,83 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.core import validators
+from django.utils.deconstruct import deconstructible
+from django.core.exceptions import ValidationError
+
+
+def user_avatar_directory_path(instance: User, filename: str) -> str:
+    """ Путь для сохранения аватара пользователя"""
+    return f"users/avatars/user_{instance.pk}/{filename}"
+
+
+def get_default_avatar_path():
+    """ Путь к аватару пользователя по умолчанию"""
+    return "users/avatars/default/default_avatar1.png"
+
+
+@deconstructible
+class PhoneNumberValidator(validators.RegexValidator):
+    """Проверка формата номера телефона"""
+    regex = r'^\+\d+$'
+    message = 'Номер телефона должен начинаться с + и содержать только цифры'
+    flags = 0
+
+
+def phone_unique_validator(value):
+    """Проверка уникальности номера телефона """
+    user = User.objects.select_related('profile').filter(profile__phone_number=value).exists()
+    if user and value != '+0000000000':
+        print(value)
+        print(type(value))
+        raise ValidationError('%s уже используется другим пользователем.' % value)
+
+
+def validate_image_size(image):
+    """Проверка допустимого размера файла"""
+    MAX_SIZE = 2 * 1024 ** 2  # 2MB
+    if image.size > MAX_SIZE:
+        raise ValidationError(f'Размер файла превышает допустимое значение 2 MB.')
+
+
+class Profile(models.Model):
+    """ Модель профиля пользователя"""
+    phone_number_validator = PhoneNumberValidator()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='user')
+    avatar = models.ImageField(
+        null=False,
+        blank=False,
+        upload_to=user_avatar_directory_path,
+        default=get_default_avatar_path,
+        validators=[validators.validate_image_file_extension,
+                    validate_image_size
+                    ],
+        help_text='Максимальный размер фала 2MB'
+    )
+    phone_number = models.CharField(max_length=20,
+                                    verbose_name='phone number',
+                                    help_text='Номер телефона должен начинаться с + и содержать только цифры',
+                                    error_messages={'unique': 'Данный номер уже зарегистрирован'},
+                                    validators=[phone_number_validator,
+                                                phone_unique_validator
+                                                ],
+                                    null=False,
+                                    default='+0000000000'
+                                    )
+
+    class Meta:
+        verbose_name = 'User profile'
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """ Создание профиля пользователя при создании нового пользователя """
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    """Сохранение профиля пользователя """
+    instance.profile.save()
--- market/users/tests/test_users_models_form.py
+++ market/users/tests/test_users_models_form.py
@@ -2,11 +2,9 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 
-from users.models import Profile
-
 
 class UserProfileTest(TestCase):
-
+    """Создание профиля"""
     @classmethod
     def setUpClass(cls):
         cls.user = User.objects.create_user(username='Test_user', password="123")
@@ -15,22 +13,18 @@ class UserProfileTest(TestCase):
     def tearDownClass(cls):
         cls.user.delete()
 
-    def setUp(self) -> None:
-        self.user.profile.phone_number = '+8988978979879'
-        self.user.save()
-
     def test_data_profile(self):
         self.client.login(username='Test_user', password="123")
         avatar = self.user.profile.avatar
+        phone = self.user.profile.phone_number
         self.assertEqual(avatar, "users/avatars/default/default_avatar1.png")
+        self.assertEqual(phone, '+0000000000')
 
 
 class RegistrationFormTest(TestCase):
-
+    """ Проверка страницы регистрации и входа """
     def setUp(self):
         self.user = User.objects.create_user(username='Admin12', password='Pass123456')
-        self.user.profile.phone_number = '+31231231231231'
-        self.user.save()
         self.url = reverse_lazy('users:users_register')
         self.data1 = {
             'username': 'test_user',
@@ -75,14 +69,10 @@ class RegistrationFormTest(TestCase):
 
         self.assertEqual(response.status_code, 302)
         login_data2 = {'username': 'Admin12',
-                      'password': 'Pass12345'
-                      }
+                       'password': 'Pass12345'
+                       }
 
         response = self.client.get(reverse_lazy('users:users_logout'))
         response = self.client.post(reverse_lazy('users:users_login'), login_data2)
         self.assertContains(response, 'Please enter a correct username and password. '
                                       'Note that both fields may be case-sensitive.')
-
-
-
-
