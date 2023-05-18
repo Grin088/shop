ea4ac60d30e3d1850118b68680495def96b825a0
Владислав Фаст <kostrub.vlad@yandex.ru>
2023-05-18 13:49:20 +0300

    Delete views.py

--- market/catalog/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-from catalog.models import MPTTCatalog
-
-
-def show_catalog(request):
-    return render(request, 'catalog.html', {'catalog': MPTTCatalog.objects.all()})
