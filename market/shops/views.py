d8df692b558afd87857f9c6e93d40137de018f93
Zhibina <taisj281936@gmail.com>
2023-05-22 19:34:28 +0300

    changed view

--- market/shops/views.py
+++ market/shops/views.py
@@ -13,4 +13,4 @@ def home(request):
     context = {
         'random_banners': random_banners,
     }
-    return render(request, 'base.html', context)  # пока нет шаблона banner, тут будет base
+    return render(request, 'market/banner.jinja2', context)  # пока нет шаблона banner, тут будет base
