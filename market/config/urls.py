c3d1044f5eccecd698bc0f4ef5541489a0b07862
Nadorozhnyy <59502289+Nadorozhnyy@users.noreply.github.com>
2023-05-26 14:28:55 +0300

    fix templates

--- market/config/urls.py
+++ market/config/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include('users.urls')),
-    path("", include('shops.urls'))
+    path("", include('shops.urls')),
+    path("catalog/", include('product_catalog.urls')),
+    path("products/", include('products.urls'))
 ]
