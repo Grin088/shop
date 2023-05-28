0292bd1282f3257cf11df8516ba7bb7b2f6067a8
Nadorozhnyy <59502289+Nadorozhnyy@users.noreply.github.com>
2023-05-28 19:19:34 +0300

    fix reformat code

--- market/products/views.py
+++ market/products/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render  # noqa F401
 from django.views.generic import TemplateView
 # Create your views here.
 
+
 class BaseView(TemplateView):
     template_name = 'market/base.jinja2'
