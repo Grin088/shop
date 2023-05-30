b5b315c0dea6a430af8581cc9e33a83dcb227d05
??????? ??????? <Evgeniykurdeev@mail.ru>
2023-05-30 17:04:38 +0300

    Исправил имя поля в модели Product

--- market/products/models.py
+++ market/products/models.py
@@ -23,7 +23,7 @@ class Product(models.Model):
     preview = models.ImageField(null=True, blank=True, upload_to=product_preview_directory_path,
                                 verbose_name=_('предварительный просмотр'))
     property = models.ManyToManyField("Property", through="ProductProperty", verbose_name=_("характеристики"))
-    category_id = TreeForeignKey("catalog.Catalog", on_delete=models.PROTECT, null=True, related_name='category',
+    category = TreeForeignKey("catalog.Catalog", on_delete=models.PROTECT, null=True, related_name='products',
                                  verbose_name='категория')
 
     def __str__(self):
