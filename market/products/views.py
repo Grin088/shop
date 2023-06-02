ef843dec67d9c521a801b37cb6196d727dab97ed
Grin_9 <Grin_9@mail.ru>
2023-06-03 01:00:21 +1200

    add-view-css-parameters

--- market/products/views.py
+++ market/products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect # noqa F401
 from django.views.generic import TemplateView, CreateView
 from rest_framework.views import APIView
 from django.http import JsonResponse
-from products.models import Review
+from products.models import Review, Product
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from .forms import ReviewFrom
 
@@ -13,7 +13,7 @@ class ReviewsAPI(APIView):
         product_id = request.GET.get("product_id")
         offset = int(request.GET.get("offset", 0))
         limit = int(request.GET.get("limit", 3))
-        reviews = Review.objects.filter(product_id=product_id)[offset:offset + limit]
+        reviews = Review.objects.exclude(customer=request.user.id).filter(product_id=product_id)[offset:offset + limit]
         data = [{"user": r.customer.email, "product": r.product.name, "rating": r.rating, "text": r.review_text} for r in reviews]
         return JsonResponse({"data": data})
 
@@ -24,11 +24,15 @@ class ProductReviewsView(TemplateView):
 
     def get_context_data(self, **kwargs):
         product_id = self.kwargs.get('product_id')
+        product = Product.objects.get(id=product_id)
+        reviews = product.count_reviews()
         can_add_review = Review.objects.select_related('customer').select_related('product').filter(customer=self.request.user.id, product_id=product_id).first()
         context = super().get_context_data(**kwargs)
         context['product_id'] = product_id
         context['form'] = self.form_class()
         context['can_add_review'] = can_add_review
+        context['reviews'] = reviews
+        context['user'] = self.request.user
         return context
 
     def post(self, request, product_id):
