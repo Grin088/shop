64982cfde540f3fddc7bdc4cbcc859863b76bb67
Zhibina <taisj281936@gmail.com>
2023-06-04 14:39:39 +0300

    changed view

--- market/shops/views.py
+++ market/shops/views.py
@@ -4,8 +4,8 @@ from django.views.decorators.cache import cache_page
 from django.views.generic import TemplateView
 from .services import banner
 from .services.catalog import get_featured_categories
-# from .services.limited_products import get_random_limited_edition_product
-from .services.limited_products import time_left  # пока не может использоваться из-за celery
+from .services.limited_products import get_random_limited_edition_product
+# from .services.limited_products import time_left  # пока не может использоваться из-за celery
 
 
 @cache_page(settings.CACHE_CONSTANT)
@@ -13,15 +13,15 @@ def home(request):
     if request.method == "GET":
         featured_categories = get_featured_categories()
         random_banners = banner.banner()
-        time_and_products = time_left()  # пока не может использоваться из-за celery
-        update_time = time_and_products['time_left']  # пока не может использоваться из-за celery
-        limited_products = time_and_products['limited_products']  # пока не может использоваться из-за celery
-        # limited_products = get_random_limited_edition_product()
+        # time_and_products = time_left()  # пока не может использоваться из-за celery
+        # update_time = time_and_products['time_left']  # пока не может использоваться из-за celery
+        # limited_products = time_and_products['limited_products']  # пока не может использоваться из-за celery
+        limited_products = get_random_limited_edition_product()
 
         context = {
             'featured_categories': featured_categories,
             'random_banners': random_banners,
-            'update_time': update_time,  # пока не может использоваться из-за celery
+            # 'update_time': update_time,  # пока не может использоваться из-за celery
             'limited_products': limited_products,
         }
         print()
