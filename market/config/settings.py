8ea795fcd145ae26fcaa6955752661f3257729f6
Nadorozhnyy <59502289+Nadorozhnyy@users.noreply.github.com>
2023-06-10 11:19:35 +0300

    change redis location for cache and delete not used template

--- market/config/settings.py
+++ market/config/settings.py
@@ -153,7 +153,7 @@ CACHE_CONSTANT = 600
 CACHES = {
     'default': {
         'BACKEND': "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": REDIS_URL,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
