d47663b7c8bd8f7bdc9c3376641d8a6def8c502f
Vlad Fast <kostrub.vlad@yandex.ru>
2023-06-12 12:46:38 +0300

    Удаление не нужного

--- market/product_catalog/urls.py
+++ market/product_catalog/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from product_catalog.views import ViewShows, ProductDetailView
+from product_catalog.views import ViewShows
 
 urlpatterns = [
     path('', ViewShows.as_view(), name='show_product'),
-    # path('detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 
 ]
