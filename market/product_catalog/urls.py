61da8c6cdc6ceb23ffe5a68d146add7b1082cd3c
Vlad Fast <kostrub.vlad@yandex.ru>
2023-06-12 12:45:27 +0300

    Переход на детальную страницу

--- market/product_catalog/urls.py
+++ market/product_catalog/urls.py
@@ -4,6 +4,6 @@ from product_catalog.views import ViewShows, ProductDetailView
 
 urlpatterns = [
     path('', ViewShows.as_view(), name='show_product'),
-    path('detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    # path('detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 
 ]
