4e5cc76a7d844f831b24181195090b8f4ad2e086
Vlad Fast <kostrub.vlad@yandex.ru>
2023-06-22 15:20:24 +0300

    Каталог продуктов:(Документация)

--- market/product_catalog/forms.py
+++ market/product_catalog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 
 class ProductFilterForm(forms.Form):
+    """Форма заполнения фильтров"""
     price = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': "range-line", 'type': "text",
                                                                          'data-type': "double", 'data-min': "2000",
                                                                          'data-max': "100000", 'data-from': "5000",
