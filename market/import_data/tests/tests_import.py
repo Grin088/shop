f58fb750dc14dea61f0ef1429e9d67f86685e082
Zhibina <taisj281936@gmail.com>
2023-06-26 01:40:28 +0300

    add test for import

--- market/import_data/tests/tests_import.py
+++ market/import_data/tests/tests_import.py
@@ -1,35 +1,41 @@
+# from unittest.mock import patch
 from django.test import TestCase
-from django.core.management import call_command
-from products.models import Import, Product # импортируем модели Import и Product
+from products.models import Import, Product
+from import_data.tasks import import_products
 
 
 class ImportTestCase(TestCase):
     """класс для тестирования импорта данных"""
 
     def setUp(self):
-        # метод для подготовки данных перед каждым тестом
-        # создаем объект модели Import с именем файла для импорта
         self.import_obj = Import.objects.create(source='test_import.json')
 
     def test_import_data(self):
-        """метод для проверки работы импорта данных"""
-
-        # вызываем команду для запуска импорта с указанным именем файла
-        call_command('import_data', self.import_obj.source)
-
-        # получаем обновленный объект модели Import из базы данных
+        result = import_products(self.import_obj.source, 'test@example.com')
+        self.assertEqual(result, f'Импорт из {self.import_obj.source} успешно завершен. Импортировано 2 товаров.')
         self.import_obj.refresh_from_db()
-
-        # проверяем, что статус импорта изменился на "Выполнен"
         self.assertEqual(self.import_obj.status, 'completed')
-
-        # проверяем, что количество импортированных товаров равно 3
         self.assertEqual(self.import_obj.imported_count, 3)
-
-        # проверяем, что список ошибок пустой
         self.assertFalse(self.import_obj.errors)
-
-        # проверяем, что в базе данных появились товары из файла
         self.assertTrue(Product.objects.filter(name='Ноутбук ASUS VivoBook 15').exists())
         self.assertTrue(Product.objects.filter(name='Смартфон Samsung Galaxy S21').exists())
         self.assertTrue(Product.objects.filter(name='Планшет Apple iPad Air').exists())
+
+
+# class TestImportProducts(TestCase):
+#     def testimportproducts(self):
+#         with open('testimport.json', 'w') as f:
+#             f.write(
+#                 '[{"name": "Product 1", "description": "Description 1",'
+#                 ' "limitededition": false, "preview": "", "category": "Category 1"},'
+#                 ' {"name": "Product 2", "description": "Description 2",'
+#                 ' "limitededition": false, "preview": "", "category": "Category 1"}]')
+#
+#         with patch('django.core.mail.send_mail') as mocksendmail:
+#             result = import_products.delay('testimport.json', 'test@example.com').get()
+#
+#         self.assertIn('Импорт из testimport.json успешно завершен. Импортировано 2 товаров.', result)
+#         self.assertEqual(mocksendmail.callcount, 1)
+#         self.assertEqual(mocksendmail.callargs[0][1],
+#                          'Импорт товаров из файла testimport.json был успешно выполнен.
+#                          \nПодробности импорта можно посмотреть в файле')
