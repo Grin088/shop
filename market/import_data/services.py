ab62763620608b39741ed4f5c5635fb4c48bdbaa
Zhibina <taisj281936@gmail.com>
2023-06-28 20:23:03 +0300

    add services file

---
+++ market/import_data/services.py
@@ -0,0 +1,38 @@
+import json
+import os
+from django.conf import settings
+from products.models import Import, Product
+from catalog.models import Catalog
+
+
+def process_products(file_path, email=None):
+    with open(file_path) as f:
+        data = json.load(f)
+    log_file_name = os.path.basename(file_path) + '.log'
+    log_file_path = os.path.join(settings.IMPORT_LOGS, log_file_name)
+    with open(log_file_path, 'w') as log_file:
+        errors = []
+        products = []  # создаем пустой список для товаров
+        for item in data:
+            name = item.get('name')
+            description = item.get('description')
+            limited_edition = item.get('limited_edition')
+            preview = item.get('preview')
+            category = item.get('category')
+            product, created = Product.objects.get_or_create(name=name, defaults={
+                'name': name, 'description': description, 'limited_edition': limited_edition})
+            if not created:
+                product.name = name
+                product.description = description
+                product.limited_edition = limited_edition
+                product.preview = preview
+                product.save()
+            category, _ = Catalog.objects.get_or_create(name=category)
+            product.category = category
+            product.save()
+            products.append(product)
+            log_file.write(f'Товар {name} был {"создан" if created else "обновлен"}\n')
+    return products, errors
+
+
+
