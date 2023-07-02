2f0cbb696d7e51ffb632a224b8b56601550e3670
Egor Tarasov <egorka.tarasov.20001@gmail.com>
2023-07-02 20:36:05 +0300

    add site settings

--- market/site_settings/views.py
+++ market/site_settings/views.py
@@ -1,11 +1,8 @@
-from django.contrib import messages
 from django.core.cache import cache
-from django.shortcuts import redirect
 from django.http import JsonResponse
 
 
 def clear_all_cache_view(request):
     """Очистка всего кеша"""
     cache.clear()
-    # messages.success(request, 'All cache is cleared')
     return JsonResponse({'status': 'ok'})
