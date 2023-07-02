3f5db35e10309a036e1dac71623c53d1b8386a43
Egor Tarasov <egorka.tarasov.20001@gmail.com>
2023-07-02 20:58:42 +0300

    add gettext

--- market/site_settings/models.py
+++ market/site_settings/models.py
@@ -24,31 +24,33 @@ class SiteSettings(OneObjectModel):
 
     # Пагинация
     pagination_size = models.PositiveIntegerField(validators=[MaxValueValidator(8)], default=8,
-                                                  verbose_name='pagination size')
+                                                  verbose_name=_('размер страницы'))
     # Главная страница
     banners_count = models.PositiveIntegerField(validators=[MaxValueValidator(3)], default=3,
-                                                verbose_name='banners count')
+                                                verbose_name=_('количество баннеров'))
     deal_of_the_day = models.PositiveIntegerField(validators=[MaxValueValidator(1)], default=1,
-                                                  verbose_name='deal of the day quantity')
+                                                  verbose_name=_('количество сделок дня'))
     hot_deals_slider = models.PositiveIntegerField(validators=[MaxValueValidator(9)], default=9,
-                                                   verbose_name='hot deals slider')
+                                                   verbose_name=_('слайдер горячих предложений'))
     top_elements_count = models.PositiveIntegerField(validators=[MaxValueValidator(8)], default=8,
-                                                     verbose_name='top elements count')
+                                                     verbose_name=_('количество элементов в топе'))
     limited_edition_count = models.PositiveIntegerField(validators=[MaxValueValidator(16)], default=16,
-                                                        verbose_name='limited edition count')
+                                                        verbose_name=_('ограниченное тираж'))
     # Детальная страница товара
     maximum_number_of_viewed_products = models.PositiveIntegerField(validators=[MaxValueValidator(20)], default=20,
-                                                                    verbose_name='maximum number of viewed products')
+                                                                    verbose_name=_(
+                                                                    'максимальное количество просмотренных товаров'))
     # Способ доставки
     free_shipping_min_order_amount = models.DecimalField(max_digits=6, decimal_places=2, default=100.00,
-                                                         verbose_name='free shipping min order amount, $')
+                                                         verbose_name=_(
+                                                             'минимальная сумма заказа для бесплатной доставки, $'))
     standard_shipping_price = models.DecimalField(max_digits=6, decimal_places=2, default=10.00,
-                                                  verbose_name='standard shipping price, $')
+                                                  verbose_name=_('стандартная цена доставки, $'))
     express_shipping_price = models.DecimalField(max_digits=6, decimal_places=2, default=25.00,
-                                                 verbose_name='express shipping price, $')
+                                                 verbose_name=_('стоимость экспресс-доставки, $'))
     # Кэш
     cache_time = models.PositiveIntegerField(validators=[MaxValueValidator(3)], default=1,
-                                             verbose_name='cache time, days')
+                                             verbose_name=_('время кэширования, дни'))
 
     def __str__(self) -> str:
         return "Site settings"
