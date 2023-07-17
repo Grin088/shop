d86fabf4232f495b095e80ea09536c69918ebd45
Nadorozhnyy <59502289+Nadorozhnyy@users.noreply.github.com>
2023-07-17 08:51:55 +0300

    fixes for flake8

--- market/cart/views.py
+++ market/cart/views.py
@@ -1,12 +1,8 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import TemplateView
-from django.views.generic import ListView
-from .models import Cart, CartItem
-from .forms import CartQuantity
+
 from shops.models import Offer
 from cart.cart import Cart as CartServices
-from django.views.decorators.http import require_POST
-from django.views.generic import TemplateView
 
 
 class CartView(TemplateView):
@@ -35,9 +31,3 @@ def delete_item_from_cart(request, pk):
     offer = get_object_or_404(Offer, id=pk)
     cart.delete_from_cart(offer=offer)
     return redirect(request.META.get('HTTP_REFERER'))
-
-
-
-
-
-
