704cd53e7b17b11f1e009132a081c278275450c8
Vlad Fast <kostrub.vlad@yandex.ru>
2023-07-31 19:19:24 +0300

    Корректный вывод фильтра и сортировки по ценам со скидкой

--- market/catalog/services.py
+++ market/catalog/services.py
@@ -1,7 +1,6 @@
 from django.core.paginator import Paginator
-from django.db.models import Q, Min
+from django.db.models import Q, Min, Avg
 from django.shortcuts import render
-
 from catalog.forms import ProductFilterForm
 from products.models import Product
 from shops.services.compare import compare_list_check
@@ -9,7 +8,6 @@ from shops.services.compare import compare_list_check
 
 def get_paginator(request, products, forms):
     """Представление пагинации 'Каталог продуктов' и сессия для сортировки"""
-
     if request.GET.get("sort"):
         request.session["sorted"] = request.GET.get("sort")
         sort = request.session["sorted"]
@@ -31,12 +29,15 @@ def get_paginator(request, products, forms):
 
 def sorted_products(sort, product):
     """Сортировка по критериям"""
-    if sort in ("offers__price", "-offers__date_of_creation"):
+    if sort in "offers__price":
+        product = product.annotate(average_stars=Avg('offers__discount_price')).order_by('average_stars')
+        return product
+    elif sort in '-offers__date_of_creation':
         product = {
             products: price
             for products in product.order_by(sort)
             for price in products.offers.aggregate(
-                item=Min("price" if sort == "offers__price" else "date_of_creation")
+                item=Min("date_of_creation")
             )
         }
         product = list(product.keys())
@@ -55,39 +56,47 @@ def sorted_products(sort, product):
         return product
 
 
+def check_discount_price(products):
+    """Обновление цены со скидкой"""
+    for product in products:
+        for ii in product.offers.all():
+            ii.discount_price = ii.price_with_discount
+            ii.save()
+
+
 def filter_search(session, products):
     """Фильтрация продуктов"""
     prices = session["price"].split(";")
     sessions = {value: key for value, key in session.items() if (session[value] and value != "price")}
-    product_search = products.filter(
+    product_search = products.annotate(discount=Avg('offers__discount_price')).filter(
         (
             Q(name__icontains="" if sessions.get("name") is None else sessions["name"])
-            & Q(offers__price__range=(prices[0], prices[1]))
+            & Q(discount__range=(prices[0], prices[1]))
         )
     )
 
     if sessions.get("in_stock"):
-        product_search = products.filter(
+        product_search = products.annotate(discount=Avg('offers__discount_price')).filter(
             (
                 Q(name__icontains="" if sessions.get("name") is None else sessions["name"])
-                & Q(offers__price__range=(prices[0], prices[1]))
+                & Q(discount__range=(prices[0], prices[1]))
             )
             & Q(offers__product_in_stock=None if sessions.get("in_stock") is None else sessions["in_stock"])
         )
 
     if sessions.get("free_delivery"):
-        product_search = products.filter(
+        product_search = products.annotate(discount=Avg('offers__discount_price')).filter(
             (
                 Q(name__icontains="" if sessions.get("name") is None else sessions["name"])
-                & Q(offers__price__range=(prices[0], prices[1]))
+                & Q(discount__range=(prices[0], prices[1]))
             )
             & Q(offers__free_shipping=None if sessions.get("free_delivery") is None else sessions["free_delivery"])
         )
     if sessions.get("in_stock") and sessions.get("free_delivery"):
-        product_search = products.filter(
+        product_search = products.annotate(discount=Avg('offers__discount_price')).filter(
             (
                 Q(name__icontains="" if sessions.get("name") is None else sessions["name"])
-                & Q(offers__price__range=(prices[0], prices[1]))
+                & Q(discount__range=(prices[0], prices[1]))
             )
             & Q(offers__free_shipping=None if sessions.get("free_delivery") is None else sessions["free_delivery"])
             & Q(offers__product_in_stock=None if sessions.get("in_stock") is None else sessions["in_stock"])
@@ -97,7 +106,6 @@ def filter_search(session, products):
 
 class MixinGetPost:
     """Представление 'Каталог продуктов'"""
-
     def get(self, request):
         if "filter" in request.session:
             sessions = request.session["filter"]
@@ -110,11 +118,13 @@ class MixinGetPost:
         else:
             form = ProductFilterForm()
             products = Product.objects.all()
+            check_discount_price(products=products)
         context = get_paginator(request, products, form)
         return render(request, "market/catalog/catalog.jinja2", context=context)
 
     def post(self, request):
         product = Product.objects.all()
+        check_discount_price(products=product)
         form = ProductFilterForm(request.POST)
         if form.is_valid():
             prices = form.cleaned_data["price"].split(";")
