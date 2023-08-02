359c054ff10fa7a2fd244f2d106de80174ac075b
Vlad Fast <kostrub.vlad@yandex.ru>
2023-08-03 00:36:40 +0300

    fix/ errors

--- market/catalog/price_and_discounts.py
+++ market/catalog/price_and_discounts.py
@@ -1,23 +1,23 @@
 from django.db.models import Min, Max, Avg
-
 from products.models import Product
+
+products = Product.objects.all()
 
 
 def min_price():
-    products = Product.objects.all()
-    check_discount_price(products)
-    price = products.annotate(price_min_discount=Avg('offers__discount_price')).aggregate(Min('price_min_discount'))
-    return round(price['price_min_discount__min'] or 0)
+    check_discount_price()
+    price = Product.objects.annotate(price_min_discount=Avg('offers__discount_price'))
+    price_min = price.aggregate(Min('price_min_discount'))
+    return round(price_min['price_min_discount__min' or None])
 
 
 def max_price():
-    products = Product.objects.all()
-    check_discount_price(products)
-    price = products.annotate(price_max_discount=Avg('offers__discount_price')).aggregate(Max('price_max_discount'))
-    return round(price['price_max_discount__max'] or 0)
+    price = products.annotate(price_max_discount=Avg('offers__discount_price'))
+    price_max = price.aggregate(Max('price_max_discount'))
+    return round(price_max['price_max_discount__max' or None])
 
 
-def check_discount_price(products):
+def check_discount_price():
     """Обновление цены со скидкой"""
     for product in products:
         for ii in product.offers.only('discount_price'):
